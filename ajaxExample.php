b0412de41df6a56b91bff5c47944aac86f5fd279
Sparks Michael <Sparks Michael>
2016-02-09 09:46:34 -0500

    Ajax Example With Button

--- ajaxExample.php
+++ ajaxExample.php
@@ -1,9 +1,18 @@
 $.ajax({  
 		type: 'POST',  
-		url: 'admin.php', 
-		data: { action: 'add', empNum: employeeNum, empFirst: empFirstName, empLast: empLastName, empEmail: employeeEmail, empPass: empPassword ,
-		empTimesheet: timesheet, empServiceForm: serviceForm, empZonesheet: zonesheet, empChangeOrder: changeorder, empMailingList: mailinglist,
-		empPlaceOrder: placeorder, empPayroll: payroll, empAdministrator: administrator},
+		url: 'https://api.hubapi.com/calendar/v1/events/task/600442?hapikey=demo', 
+		data: {
+          "eventDate": 1425760557000,
+          "state": "DONE",
+          "category": "BLOG_POST",
+          "campaignGuid": "f43fe9fd-4082-4a45-93d8-cb8a88f01654",
+          "name": "Test Blog Task With topics updated",
+          "description": "Cool Post with Topics",
+          "ownerId": 13316,
+          "topicIds": [
+            330075656
+          ]
+        },
 		success: function(response) {
 			alert(response);
 		}
